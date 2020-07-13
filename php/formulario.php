9b2473fb46548fd6f744447bacde87d63562a601
Murilo Rodrigues <65139977+MuriloTIKIS@users.noreply.github.com>
2020-07-13 00:59:11 -0300

    Add files via upload

---
+++ php/formulario.php
@@ -0,0 +1,29 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+    
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $mensagem = addslashes($_POST['mensagem']);
+
+    $to = "murilo2759@gmail.com";
+    $subject = "Contato - Portifólio";
+    $body = "Nome: ".$nome. "\r\n".
+            "Email: ".$email. "\r\n". 
+            "Telefone: ".$telefone. "\r\n".
+            "Mensagem: ".$mensagem;
+    $header = "From: historiesnutshell@gmail.com"."\r\n".
+                "Reply-to:".$email."\r\n".
+                "X=Mailer:PHP/".phpversion();
+
+    if(mail($to,$subject,$body,$header)){
+        
+        echo("Mensagem enviada!")
+
+    }else{
+        echo("A mensagem não pôde ser enviada! Por favor, tente novamente")
+    }            
+}
+
+?>
