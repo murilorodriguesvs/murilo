6c3c04e5836112278cd47acca139d330c3769f70
Murilo Rodrigues <65139977+MuriloTIKIS@users.noreply.github.com>
2020-07-26 16:57:10 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,28 @@
 var abrirMenu = document.querySelector('.menu-abrir');
 var fecharMenu = document.querySelector('.menu-fechar');
+var fechar = document.querySelector('#fechar');
+var fechar1 = document.querySelector('#fechar1');
+var fechar2 = document.querySelector('#fechar2');
+var fechar3 = document.querySelector('#fechar3');
 
 abrirMenu.addEventListener('click', function(){
     document.documentElement.classList.add('menu-ativo');
 });
 
 fecharMenu.addEventListener('click', function(){
+    document.documentElement.classList.remove('menu-ativo');
+});
+
+fechar.addEventListener('click', function(){
+    document.documentElement.classList.remove('menu-ativo');
+});
+fechar1.addEventListener('click', function(){
+    document.documentElement.classList.remove('menu-ativo');
+});
+fechar2.addEventListener('click', function(){
+    document.documentElement.classList.remove('menu-ativo');
+});
+fechar3.addEventListener('click', function(){
     document.documentElement.classList.remove('menu-ativo');
 });
 
